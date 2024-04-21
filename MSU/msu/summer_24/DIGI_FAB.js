dfb377076f92781c08bbda0b81b5e82450dacee4
aubreyalfonzo <aubrey.j.pohl@gmail.com>
2024-04-20 23:05:55 -0500

    Update DIGI_FAB.js

--- MSU/msu/summer_24/DIGI_FAB.js
+++ MSU/msu/summer_24/DIGI_FAB.js
@@ -19,17 +19,21 @@ function preload(){
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(200);
+	background(242);
 	imageMode(CENTER);
 	textFont(font);
 	fill(46, 49, 146);
 	textAlign(LEFT, TOP);
+	pixelDensity(1);
 	
-	div = createDiv('Second Summer Session / July 8th-August 6th, 2024'+ '<br>' + 'Professors Caroline Hatfield & Aubrey Pohl' + '<br><br>' + 'This course will provide introductory instruction in digital fabrication as it relates to object design and sculpture. We will cover 3D Modeling in Fusion 360 and Blender, laser cutting, 3D printing, and fabricating metal forms from CNC plasma cut components. Lectures will cover contemporary art and design utilizing digital fabrication. Assignments will prioritize creative/conceptual intent alongside technical development. Student choice-based learning will anticipate audience as either consumer or viewer to engage in graphic design, product design, and contemporary art making. For GD majors, this course can count as either a concentration elective or sculpture survey. When registering for this course, register for Art 4753 Scupl Mat & Processes.');
+	div = createDiv('<strong>2nd Summer Session / July 8th-August 6th, 2024'+ '<br>' + 'Professors Caroline Hatfield & Aubrey Pohl</strong>' + '<br><br>' + 'This course will provide introductory instruction in digital fabrication as it relates to object design and sculpture. We will cover 3D Modeling in Fusion 360 and Blender, laser cutting, 3D printing, and fabricating metal forms from CNC plasma cut components. Lectures will cover contemporary art and design utilizing digital fabrication. Assignments will prioritize creative/conceptual intent alongside technical development. Student choice-based learning will anticipate audience as either consumer or viewer to engage in graphic design, product design, and contemporary art making. For GD majors, this course can count as either a concentration elective or sculpture survey. When registering for this course, register for Art 4753 Scupl Mat & Processes.');
 	
 }
 
 function draw() {
+	let mm = map(frameCount, 0, 2024, .1, 1);
+	pixelDensity(mm);
+
 	background(242);
 	
 	if(width > 1080){
@@ -43,7 +47,7 @@ function draw() {
 	div.position(25, width/3.5);
 	div.style('color', '#2e3192');
 	div.style('background-color', '#f2f2f2');
-	div.style('padding', '5px');
+	div.style('padding', '10px');
 	div.style('font-size', '15pt');
 	
 	textSize(width/7);
@@ -78,6 +82,13 @@ function draw() {
 	image(digi, width/2+m+mx, height/2-m-mx);
 	image(fab, width/2-m-mx, height/2+m+mx);
 	pop();
+
+
+	if(frameCount > 2024){
+		frameCount = 1;
+	}
+
+	print(frameCount);
 	
 	
 }
