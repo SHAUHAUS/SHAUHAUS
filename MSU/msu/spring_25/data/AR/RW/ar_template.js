2d573fd53b5812eed1b6a72c2f1cd95d4d04ea03
aubreyalfonzo <aubrey.j.pohl@gmail.com>
2025-02-19 12:33:15 -0600

    BIG OLE AR UPDATE

---
+++ MSU/msu/spring_25/data/AR/RW/ar_template.js
@@ -0,0 +1,38 @@
+let img, img1, img2;
+// add image variables as needed perlayer
+
+let arLayers = []
+
+function preload(){
+	img = loadImage('1.png');
+	img1 = loadImage('2.png');
+	img2 = loadImage('3.png');
+	// add image loads as needed per layer
+}
+
+function setup() {
+	noCanvas();
+	pixelDensity(1) // prevent 200+ PPI lag
+	// colorMode(HSL)
+	
+//	img.resize(895, 1350);
+//	img1.resize(895, 1350);
+//	img2.resize(895, 1350);
+	//add image resizes per layer as needed
+}
+
+function draw(){
+
+	arLayers[0] = createGraphics(895, 1350, document.getElementById('canvas-ar')) // poster aspect
+	arLayers[1] = createGraphics(895, 1350, document.getElementById('canvas-ar2')) // poster aspect
+	arLayers[2] = createGraphics(895, 1350, document.getElementById('canvas-ar3')) // poster aspect
+
+	arLayers[0].image(img2, 0, 0);
+	arLayers[1].image(img1, 0, 0);
+	arLayers[2].image(img, 0, 0);
+	//add layers as needed, make sure to match in html
+	
+	
+
+}
+
